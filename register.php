43f3ae87770e7530b418886795a0b92f0e0f0aac
Sir-JamesMuritu <jemo7158@gmail.com>
2023-08-16 14:10:39 +0300

    modified the files

--- register.php
+++ register.php
@@ -1,3 +1,7 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
